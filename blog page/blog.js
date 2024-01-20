512d30fcd5c405aa088f3d398965da8ff5a51f45
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2024-01-20 19:53:29 +0530

    tried and error with Blog updated time , current time

--- blog page/blog.js
+++ blog page/blog.js
@@ -1,1 +1,20 @@
 // alert('connected')
+// document.getElementById('btn').addEventListener(DOMContentLoaded, function(){
+//  document.getElementById('btn').addEventListener('click',function(){
+
+//     let currentDate = new Date();
+//     console.log(currentDate);
+//     console.log(presentDate);
+// })
+
+// })
+
+// alert('connected')
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn').addEventListener('click', function () {
+        let currentDate = new Date();
+        console.log(currentDate);
+        // Assuming presentDate is defined elsewhere in your code
+        console.log(presentDate);
+    });
+});
